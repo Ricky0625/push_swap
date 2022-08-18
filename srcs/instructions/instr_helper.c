06ce8bc714cfbc4943e28c9aa76fe620c1d0989b
Ricky Wong <wricky-t@student.42kl.edu.my>
2022-08-18 21:37:39 +0800

    added push_n_item

--- srcs/instructions/instr_helper.c
+++ srcs/instructions/instr_helper.c
@@ -6,7 +6,7 @@
 /*   By: wricky-t <wricky-t@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 15:53:40 by wricky-t          #+#    #+#             */
-/*   Updated: 2022/08/18 19:59:23 by wricky-t         ###   ########.fr       */
+/*   Updated: 2022/08/18 20:59:43 by wricky-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ void	ft_swap(int *a, int *b)
 **/
 void	push_all(t_stkgrp *stacks, int instr)
 {
-	t_stack *stack;
+	t_stack	*stack;
 
 	if (instr == PA)
 		stack = stacks->b;
@@ -41,5 +41,28 @@ void	push_all(t_stkgrp *stacks, int instr)
 	else
 		return ;
 	while (is_empty(stack) != 1)
-		p_instr(stacks, instr);	
+		p_instr(stacks, instr);
+}
+
+/**
+ * Push n item from a stack to another based on the instruction
+ * 
+ * stack	: the origin stack
+ * new_btm	: the new bottom index
+ * while the origin stack is not empty and the bottom index of stack
+ * is not the new bottom index, keep pushing
+**/
+void	push_n_item(t_stkgrp *stacks, int instr, int n)
+{
+	t_stack	*stack;
+	int		new_btm;
+
+	stack = NULL;
+	if (instr == PA)
+		stack = stacks->b;
+	else if (instr == PB)
+		stack = stacks->a;
+	new_btm = stack->btm - n;
+	while (is_empty(stack) != 1 && stack->btm != new_btm)
+		p_instr(stacks, instr);
 }
