f74fd4685b44d27663ef7df8db14aa6864665cd5
Ricky Wong <wricky-t@student.42kl.edu.my>
2022-08-19 18:31:33 +0800

    Updated sort small
    - Sort 2, 3 working
    - 4 not working
    - 5 working but maximum action exceed one

--- srcs/sorter/sort_small.c
+++ srcs/sorter/sort_small.c
@@ -6,17 +6,17 @@
 /*   By: wricky-t <wricky-t@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 20:21:41 by wricky-t          #+#    #+#             */
-/*   Updated: 2022/08/18 21:17:15 by wricky-t         ###   ########.fr       */
+/*   Updated: 2022/08/19 18:20:23 by wricky-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-void	sort_three(t_stkgrp *stacks, t_stack *main)
+void sort_three(t_stkgrp *stacks, t_stack *main)
 {
-	int		top;
-	int		mid;
-	int		btm;
+	int top;
+	int mid;
+	int btm;
 
 	top = main->items[0];
 	mid = main->items[1];
@@ -39,15 +39,57 @@ void	sort_three(t_stkgrp *stacks, t_stack *main)
 	}
 }
 
-void	sort_five(t_stkgrp *stacks)
+void sort_five(t_stkgrp *stacks)
 {
-	t_stack	*a;
-	// t_stack	*b;
+	t_stack *a;
+	t_stack *b;
+	int top_b;
+	int loc;
+	int *nruns;
 
 	a = stacks->a;
-	// b = stacks->b;
-	// push 2 item to stack b no matter what
-	push_n_item(stacks, PB,2);
-	// sort the stack a using sort_three since it has three item
+	b = stacks->b;
+	// n should not only be 2, if this works on other numbers of arguments
+	push_n_item(stacks, PB, 2);
 	sort_three(stacks, a);
+	// it's working (YAY!), just need to cut down few instructions
+	while (is_empty(b) != 1)
+	{
+		// galloping starts here, basically means push the runs to stack a
+		top_b = peek(b);
+		nruns = detect_runs(b);
+		// if natural runs exists, check where it should be placed
+		// if (nruns != NULL)
+		// {
+		// 	// printf("Natural runs exists!\nRuns starts at %d & ends at: %d\n", nruns[0], nruns[1]);
+		// 	// natural runs starts at index 0 anyway, technically it's top_b
+		// 	loc = binary_search(a, top_b, 0, a->btm);
+		// 	while (nruns[1] > 0)
+		// 	{
+		// 		// printf("doing galloping\n");
+		// 		printf("location: %d\n", loc);
+		// 		printf("is bigger? %d\n", loc < a->btm && a->items[loc + 1] > b->items[nruns[1]]);
+		// 		exit(1);
+		// 		if (loc < a->btm && a->items[loc + 1] > b->items[nruns[1]])
+		// 		{
+		// 			int n = nruns[1] - nruns[0] + 1;
+		// 			give_way(b, a, loc);
+		// 			push_n_item(stacks, PA, n);
+		// 			break;
+		// 		}
+		// 		else
+		// 			nruns[1]--;
+		// 	}
+		// }
+		// else
+		// {
+		// one by one merging process starts here
+		// probably should detect run first
+		loc = binary_search(a, top_b, 0, a->btm);
+		give_way(b, a, loc);
+		p_instr(stacks, PA);
+		// }
+		restorer(a);
+		// print_stack(stacks, 0);
+	}
 }
--- srcs/sorter/sort_utils.c
+++ srcs/sorter/sort_utils.c
@@ -6,7 +6,7 @@
 /*   By: wricky-t <wricky-t@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 21:20:03 by wricky-t          #+#    #+#             */
-/*   Updated: 2022/08/18 21:37:55 by wricky-t         ###   ########.fr       */
+/*   Updated: 2022/08/19 17:59:18 by wricky-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,12 +38,12 @@
  * Else, do binary_search from top to mid -1. (meaning target is smaller
  * than the item at midpoint)
 **/
-int	binary_search(t_stack *stack, int target, int top, int btm)
+int	binary_search(t_stack *dest, int target, int top, int btm)
 {
 	int	mid;
 	int	*items;
 
-	items = stack->items;
+	items = dest->items;
 	if (btm <= top)
 	{
 		if (target > items[top])
@@ -54,11 +54,149 @@ int	binary_search(t_stack *stack, int target, int top, int btm)
 	if (target == items[mid])
 		return (mid + 1);
 	if (target > items[mid])
-		return (binary_search(stack, target, mid + 1, btm));
-	return (binary_search(stack, target, top, mid - 1));
+		return (binary_search(dest, target, mid + 1, btm));
+	return (binary_search(dest, target, top, mid - 1));
 }
 
-// void	rotate_decider(t_stack *dest)
-// {
-	
-// }
+/**
+ * Detect natural runs in a stack
+ * 
+ * Natural runs: Already-ordered elements like 1,2,4,6,8,9 or in descending
+ * For this function, we only detect natural runs in descending order
+ * The function will return an array of two index value if there is a run.
+ * nruns = {start, end};
+ * 
+ * Will assume the start of a natural run start from index 0, so nruns[0] = 0.
+ * Iterate through the items, as long as it's less than the target's btm
+ * If item at index is less than item at index + 1, meaning it's an ascending
+ * order run, break the loop.
+ * Check if the index is greater than 0, if yes, set natural run ending at index.
+ * Else return NULL, meaning to natural run.
+**/
+int	*detect_runs(t_stack *target)
+{
+	int	index;
+	int	*items;
+	int	*nruns;
+
+	index = 0;
+	items = target->items;
+	nruns = malloc(sizeof(int) * 2);
+	nruns[0] = 0;
+	while (index < target->btm)
+	{
+		if (items[index] < items[index + 1])
+			break ;
+		index++;
+	}
+	if (index > 0)
+		nruns[1] = index;
+	else
+	{
+		free(nruns);
+		return (NULL);
+	}
+	return (nruns);
+}
+
+/**
+ * Find the smallest item in the stack and return it's position (index)
+ * 
+ * Assume the smallest item is on the top. Iterate through the items,
+ * if there is an item that is smaller than the smallest (what we assume),
+ * update smallest to the item's index.
+ * 
+ * Return smallest, which is the index of smallest item at the end.
+**/
+int	find_smallest(t_stack *stack)
+{
+	int	*items;
+	int	index;
+	int	smallest;
+
+	items = stack->items;
+	index = 1;
+	smallest = 0;
+	while (index <= stack->btm)
+	{
+		if (items[index] < items[smallest])
+			smallest = index;
+		index++;
+	}
+	return (smallest);
+}
+
+/**
+ * Give way. The function name explains it all, to give way for the new item
+ * 
+ * dest_btm		: destination btm index
+ * dist_to_top	: distance between location to top
+ * dist_to_btm	: distance between location to btm
+ * 
+ * 1. If location == 0 (new smallest), or location > dest_btm (new largest) do
+ * 	  nothing, can push afterward and restorer will handle the rest
+ * 2. If dist_to_top < dist_to_btm, do RA dist_to_top times
+ * 3. If dist_to_btm < dist_to_top, do RRA dist_to_btm times
+ * 4. When dist_to_top == dist_top_btm, no matter what direction, will be the
+ *    same, so either one. For this function, I choose to do RA.
+**/
+// need to remove org since no use
+void	give_way(t_stack *org, t_stack *dest, int location)
+{
+	int	dest_btm;
+	int	dist_to_top;
+	int	dist_to_btm;
+
+	dest_btm = dest->btm;
+	(void)org;
+	if (location > dest_btm || location == 0)
+		return ;
+	if (location == dest_btm)
+	{
+		rotate_stk(dest, RRA);
+		return ;
+	}
+	dist_to_top = location;
+	dist_to_btm = dest_btm - location + 1;
+	if (dist_to_top <= dist_to_btm)
+		while (--dist_to_top >= 0)
+			rotate_stk(dest, RA);
+	if (dist_to_btm < dist_to_top)
+		while (--dist_to_btm >= 0)
+			rotate_stk(dest, RRA);
+}
+
+/**
+ * Restore the stack back to ascending order
+ * 
+ * smallest		: the index of the smallest item
+ * dest_btm		: btm index of the stack
+ * dist_to_top	: distance between smallest item to top
+ * dist_to_btm	: distance between smallest item to btm
+ * 
+ * 1. if smallest is == 0, meaning smallest ady at top, don't move
+ * 2. If dist_to_top < dist_to_btm, do RA dist_to_top times
+ * 3. If dist_to_btm < dist_to_top, do RRA dist_to_btm times
+ * 4. When dist_to_top == dist_top_btm, no matter what direction, will be the
+ *    same, so either one. For this function, I choose to do RA.
+**/
+void	restorer(t_stack *stack)
+{
+	int	smallest;
+	int	dest_btm;
+	int	dist_to_top;
+	int	dist_to_btm;
+
+	smallest = find_smallest(stack);
+	dest_btm = stack->btm;
+	if (smallest == 0)
+		return ;
+	dist_to_top = smallest;
+	dist_to_btm = dest_btm - smallest + 1;
+	if (dist_to_top < dist_to_btm)
+		while (--dist_to_top >= 0)
+			rotate_stk(stack, RA);
+	if (dist_to_btm <= dist_to_top)
+		while (--dist_to_btm >= 0)
+			rotate_stk(stack, RRA);
+}
